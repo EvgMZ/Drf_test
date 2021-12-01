8794aabdfe19216630b2237ec9ee1c117cbdb28d
Засолкин Евгений <men6586@yandex.ru>
2021-12-01 09:52:08 +0300

    add form

--- src/scrapping/views.py
+++ src/scrapping/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
-from .models import Vacancy
+from .models import Language, Vacancy
+from .forms import FindForm
 
 
 def home_page(request):
-    qs = Vacancy.objects.all()
-    return render(request, 'scraping/index.html', context={'object_list': qs})
-
-def asd():
-    print()
+    form = FindForm()
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    print(language)
+    print(city)
+    qs = []
+    _filter = {}
+    if city or language:
+        if city:
+            _filter['city__slug'] = city
+        if language:
+            _filter['Language__slug'] = language
+    print(_filter)
+    qs = Vacancy.objects.filter(**_filter)
+    print(qs)
+    return render(
+        request,
+        'scraping/index.html',
+        context={'object_list': qs, 'form': form}
+    )
