9f5d87181822bb8fe7e471d1d51da686b8083d09
Засолкин Евгений <men6586@yandex.ru>
2022-02-16 14:41:25 +0300

    proccesing

--- src/drf_django_scrapping/settings/production.py
+++ src/drf_django_scrapping/settings/production.py
@@ -145,5 +145,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
